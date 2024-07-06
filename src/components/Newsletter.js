2c01d0fe6e94e14ebd347f582917399f6224800f
MosesSupposes <mosesasamuel633@gmail.com>
2024-07-06 14:06:00 -0400

    add a Newsletter component with controlled inputs

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,32 +1,44 @@
+import React from "react";
 
-const NewsLetter = (props) => (
-  <form>
-    <div>
-      <label>
-        Email:
-        <input
-          type="email"
-          name="userEmail"
-          placeholder="jwebber@example.com"
-          value={props.userEmail}
-          onChange={props.handleEmailChange}
-          style={{ marginLeft: "35px", border: "1px solid black" }}
-        />
-      </label>
-    </div>
-    <br />
-    <div>
-      <button
-        role="submit"
-        onClick={(event) => {
-          event.preventDefault();
-          props.setUserEmail("");
-          // TODO: Lookup user in database and redirect them to home page (to the graph)
-          // ? Maybe indicate that sign in was successful with a toast...
-        }}
-        style={{ background: "#999", padding: "9px" }}
-      >
-        Subscribe to the Folio Graph Newsletter
-      </button>
-    </div>
-  </form>
+export default function NewsLetter(props) {
+  const [userEmail, setUserEmail] = React.useState("");
+
+  const handleChange = (event) => {
+    setUserEmail(event.target.value);
+  };
+
+  return (
+    <form>
+      <div>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="userEmail"
+            placeholder="jwebber@example.com"
+            value={userEmail}
+            onChange={handleChange}
+            style={{
+              marginLeft: "35px",
+              border: "1px solid black",
+              color: "black",
+            }}
+          />
+        </label>
+      </div>
+      <br />
+      <div>
+        <button
+          role="submit"
+          onClick={(event) => {
+            event.preventDefault();
+            setUserEmail("");
+          }}
+          style={{ background: "#999", padding: "9px" }}
+        >
+          Subscribe to the Folio Graph Newsletter
+        </button>
+      </div>
+    </form>
+  );
+}
